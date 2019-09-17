86c0bc0366c0b9e116d3dc297fdc8c8faca01cf5
ECharles <1719965173@qq.com>
2019-09-17 23:23:12 +0800

    new file:   www/__pycache__/apis.cpython-37.pyc 	new file:   www/__pycache__/config.cpython-37.pyc 	new file:   www/__pycache__/config_default.cpython-37.pyc 	new file:   www/__pycache__/config_override.cpython-37.pyc 	new file:   www/__pycache__/coroweb.cpython-37.pyc 	new file:   www/__pycache__/handlers.cpython-37.pyc 	new file:   www/__pycache__/markdown2.cpython-37.pyc 	new file:   www/__pycache__/model.cpython-37.pyc 	new file:   www/__pycache__/models.cpython-37.pyc 	new file:   www/__pycache__/orm.cpython-37.pyc 	new file:   www/apis.py 	modified:   www/app.py 	new file:   www/config.py 	new file:   www/config_default.py 	new file:   www/config_override.py 	new file:   www/coroweb.py 	new file:   www/handlers.py 	new file:   www/model.py 	modified:   www/orm.py 	new file:   www/static/css/awesome.css 	new file:   www/static/js/awesome.js 	new file:   www/static/js/jquery.min.js 	new file:   www/static/js/sha1.min.js 	new file:   www/static/js/sticky.min.js 	new file:   www/static/js/vue.min.js 	new file:   www/templates/test.html 	new file:   www/test.py

---
+++ www/test.py
@@ -0,0 +1,15 @@
+import orm
+import asyncio
+from model import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='root', password='root', db='awesome')
+    u = User(name='Te32st', email='tes23t@qq.com', passwd='12345367890', image='about:blank')
+    await u.save()
+    ## 网友指出添加到数据库后需要关闭连接池，否则会报错 RuntimeError: Event loop is closed
+    orm.__pool.close()
+    await orm.__pool.wait_closed()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.close()
